e003eac7d3e059d57cb0658a92ab5ea07b1306e8
davphestark <davphestark@gmail.com>
2017-02-23 21:57:36 -0500

    Cleanup and Refactoring
    moved things around

---
+++ webscraper/LeagueTeams.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace webscraper
+{
+    public class LeagueTeams
+    {
+        public string LeagueName { get; set; }
+        public List<Team> Teams;
+        public LeagueTeams()
+        {
+            Teams = new List<Team>();
+        }
+        public LeagueTeams(string league) : this()
+        {
+            LeagueName = league;
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("http://www.bbc.com/sport/football/" + league + "/table");
+
+            HtmlNode legaueTable = doc.DocumentNode.SelectSingleNode("//table[@data-competition-slug='" + league + "']");
+            HtmlNodeCollection teamRows = legaueTable.SelectNodes("//tr[@data-team-slug]");
+            foreach (HtmlNode team in teamRows)
+            {
+                Teams.Add(new Team(team));
+            }
+        }
+    }
+
+}
