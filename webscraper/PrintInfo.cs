227cbf5559d181e7b77dd5d0ce85d45469055332
davphestark <davphestark@gmail.com>
2017-02-25 22:27:21 -0500

    refactored information print
    moved printing info out of main into seprate class and broke up print
    actions. still needs work

---
+++ webscraper/PrintInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace webscraper
+{
+    public class PrintInfo
+    {
+        public Team Leaders { get; set; }
+        public Team LastGoodSpot { get; set; }
+        public Team DropZone { get; set; }
+        public Team SearchTeam { get; set; }
+
+        public PrintInfo(string teamName, LeagueTeams League)
+        {
+            //this needs rework so the postions aren't hard coded
+            Leaders = League.Teams.Find(t => t.Position == 1);
+            LastGoodSpot = League.Teams.Find(t => t.Position == 4);
+            DropZone = League.Teams.Find(t => t.Position == 18);
+            SearchTeam = League.Teams.Find(t => t.Name == teamName);
+        }
+
+        public void PrintLeagueSpots()
+        {
+            Console.WriteLine(String.Format($"League leaders {Leaders.Name} are at: {Leaders.Points} points"));
+            Console.WriteLine(String.Format($"Final Champions League place is {LastGoodSpot.Name} at: {LastGoodSpot.Points} points"));
+            Console.WriteLine(String.Format($"{DropZone.Name} are top of the drop at: {DropZone.Points} points"));
+        }
+        public void PrintLeagueStatusFor()
+        {
+            Console.WriteLine(String.Format($"{SearchTeam.Name} are {(Leaders.Points - SearchTeam.Points)} points behind the leaders with {(Leaders.Played - SearchTeam.Played)} game(s) in hand"));
+            Console.WriteLine(String.Format($"{SearchTeam.Name} are {(LastGoodSpot.Points - SearchTeam.Points)} points back of the champions league with {(LastGoodSpot.Played - SearchTeam.Played)} game(s) in hand"));
+            Console.WriteLine(String.Format($"{SearchTeam.Name} are {(SearchTeam.Points - DropZone.Points)} points above the drop with {(DropZone.Played - SearchTeam.Played)} game(s) in hand"));
+        }
+        public void PrintLastTenProgressFor()
+        {
+            Console.WriteLine(String.Format("{0} {1} {2} points on {3} in the last ten games", SearchTeam.Name, (SearchTeam.LastTenPoints - Leaders.LastTenPoints) < 0 ? "loss" : "gained", (SearchTeam.LastTenPoints - Leaders.LastTenPoints), Leaders.Name));
+            Console.WriteLine(String.Format("{0} {1} {2} points on {3} in the last ten games", SearchTeam.Name, (SearchTeam.LastTenPoints - LastGoodSpot.LastTenPoints) < 0 ? "loss" : "gained", (SearchTeam.LastTenPoints - LastGoodSpot.LastTenPoints), LastGoodSpot.Name));
+            Console.WriteLine(String.Format("{0} {1} {2} points on {3} in the last ten games", SearchTeam.Name, (SearchTeam.LastTenPoints - DropZone.LastTenPoints) < 0 ? "loss" : "gained", (SearchTeam.LastTenPoints - DropZone.LastTenPoints), DropZone.Name));
+        }
+        public void PrintLastGamesInfo()
+        {
+            Console.WriteLine(String.Format($"the last ten league games gained {SearchTeam.LastTenPoints} for {SearchTeam.Name} are:"));
+            foreach (string game in SearchTeam.LastTen)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
--- webscraper/Program.cs
+++ webscraper/Program.cs
@@ -7,29 +7,13 @@ namespace webscraper
         static void Main(string[] args)
         {
             LeagueTeams PremierLeague = new LeagueTeams("premier-league");
-            PrintOut("Manchester United", PremierLeague);
+            PrintInfo p = new PrintInfo("Manchester United", PremierLeague);
+            p.PrintLeagueSpots();
+            p.PrintLeagueStatusFor();
+            p.PrintLastTenProgressFor();
+            p.PrintLastGamesInfo();
 
             Console.ReadKey();
         }
-        private static void PrintOut(string teamName, LeagueTeams League)
-        {
-            Team leaders = League.Teams.Find(t => t.Position == 1);
-            Team lastChamps = League.Teams.Find(t => t.Position == 4);
-            Team dropZone = League.Teams.Find(t => t.Position == 18);
-            Team searchedTeam = League.Teams.Find(t => t.Name == teamName);
-            Console.WriteLine(String.Format($"League leaders {leaders.Name} are at: {leaders.Points} points"));
-            Console.WriteLine(String.Format($"Final Champions League place is {lastChamps.Name} at: {lastChamps.Points} points"));
-            Console.WriteLine(String.Format($"{dropZone.Name} are top of the drop at: {dropZone.Points} points"));
-            Console.WriteLine(String.Format($"{searchedTeam.Name} are {(leaders.Points - searchedTeam.Points)} points behind the leaders"));
-            Console.WriteLine(String.Format($"{searchedTeam.Name} are {(lastChamps.Points - searchedTeam.Points)} points back of the champions league"));
-            Console.WriteLine(String.Format($"{searchedTeam.Name} are {(searchedTeam.Points - dropZone.Points)} points above the drop"));
-            Console.WriteLine();
-            Console.WriteLine(String.Format($"the last ten league games for { searchedTeam.Name} are:" ));
-            foreach (string game in searchedTeam.LastTen)
-            {
-                Console.WriteLine(game);
-            }
-        }
-
     }
 }
