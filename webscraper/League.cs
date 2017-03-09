254d8fca0bec71b90eff5ff1c85084c23fb67447
davphestark <davphestark@gmail.com>
2017-03-08 21:26:26 -0500

    Added LeagueName
    switched leagueName to an id as it has been used for that and added
    league name to give a displayable name for each league.

--- webscraper/League.cs
+++ webscraper/League.cs
@@ -7,18 +7,19 @@ namespace webscraper
 {
     public class League
     {
-        public string LeagueName { get; set; }
+        public string LeagueId { get; set; }
         public List<Team> Teams;
         public List<LeagueDivisions> Divisions;
         public bool IsTopOfPyramid;
+        public string LeagueName { get; set; }
         public League()
         {
             Teams = new List<Team>();
             Divisions = new List<LeagueDivisions>();
         }
-        public League(string league) : this()
+        public void Init(string league)
         {
-            LeagueName = league;
+            LeagueId = league;
             if (league.Contains("premier")) { IsTopOfPyramid = true; }
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load("http://www.bbc.com/sport/football/" + league + "/table");
