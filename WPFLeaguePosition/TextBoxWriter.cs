177634daf53d3c3b61a03148a0cf54928fbcbac4
davphestark <davphestark@gmail.com>
2017-03-08 21:24:41 -0500

    Added UI
    Added a WPF app to better switch leagues and teams

---
+++ WPFLeaguePosition/TextBoxWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows.Controls;
+
+namespace WPFLeaguePosition
+{
+    public class TextBoxWriter : TextWriter
+    {
+        TextBox _output = null;
+
+        public TextBoxWriter(TextBox output)
+        {
+            _output = output;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            _output.AppendText(value.ToString());
+        }
+        public override Encoding Encoding
+        {
+            get
+            {
+                return System.Text.Encoding.UTF8;
+            }
+        }
+
+    }
+}
