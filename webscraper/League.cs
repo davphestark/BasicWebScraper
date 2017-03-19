57c5349801d69f096bbf17aa0d5339ae4cbaf953
davphestark <davphestark@gmail.com>
2017-03-18 23:29:43 -0400

    refactoring
    moved things around into smaller methods for unit testing

--- webscraper/League.cs
+++ webscraper/League.cs
@@ -1,7 +1,6 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace webscraper
 {
@@ -20,25 +19,43 @@ namespace webscraper
         public void Init(string league)
         {
             LeagueId = league;
-            if (league.Contains("premier")) { IsTopOfPyramid = true; }
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load("http://www.bbc.com/sport/football/" + league + "/table");
+            IsTopOfPyramid = IsTopLeague(league);
+            HtmlNode leagueTable = LoadLeagueTable();
+            if (leagueTable != null)
+            {
+                LoadLeagueTeams(leagueTable.SelectNodes("tbody/tr[@data-team-slug]"));
+                PopulateLeagueDivisions();
+            }      
+        }
 
-            HtmlNode legaueTable = doc.DocumentNode.SelectSingleNode("//table[@data-competition-slug='" + league + "']");
-            HtmlNodeCollection teamRows = legaueTable.SelectNodes("tbody/tr[@data-team-slug]");
+        private void PopulateLeagueDivisions()
+        {
+            var leagueDivisionGuesser = new LeagueDivisionGuesser();
+            leagueDivisionGuesser.Init(this);
+            leagueDivisionGuesser.GuessDivisionsFromDividers();
+        }
+
+        private void LoadLeagueTeams(HtmlNodeCollection teamRows)
+        {
             foreach (HtmlNode team in teamRows)
             {
                 var t = new Team();
                 t.Init(team);
                 Teams.Add(t);
             }
-            var leagueDivisionGuesser = new LeagueDivisionGuesser();
-            leagueDivisionGuesser.Init(this);
-            leagueDivisionGuesser.GuessDivisionsFromDividers();
-            
         }
-        
 
+        private HtmlNode LoadLeagueTable()
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("http://www.bbc.com/sport/football/" + LeagueId + "/table");
+            return doc.DocumentNode.SelectSingleNode("//table[@data-competition-slug='" + LeagueId + "']");
+        }
+
+        public bool IsTopLeague(String league)
+        {
+            if (league.Contains("premier")) { return true; }
+            return false;
+        }
     }
-
 }
